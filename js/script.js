04f14ee781c1f30c02cf98bc821262ad90216112
weiowyw <haxjlebatop@gmail.com>
2023-08-11 17:23:18 +0500

    qwesedrf

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+
+const value = document.querySelector("#value");
+
+let count = 0;
+
+const container = document.querySelector(".container_button");
+
+const handleContainerClick = (e) => {
+    const event = e.target.dataset.action;
+
+    switch(event){
+        case 'decrease':
+            count -= 100;
+            break;
+        case 'reset':
+            count = 0;
+            break;
+        case 'increase':
+            count += 100;
+            break;
+        default:
+            return;
+    }
+    value.innerHTML = count;
+
+}
+
+container.addEventListener('click', handleContainerClick);
+
